c2913667b96ff3d31d922ad6a4f0676caa26c5d7
Onesinus SPT <onesinus231@gmail.com>
2020-03-07 07:16:24 +0700

    Change fetch to axios to getting data from api

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-export default function getLatest() {
-    fetch('https://api.exchangeratesapi.io/latest')
-        .then(reponse => {
-            return response.json();
-        })
-        .then(exchanges => {
-            return exchanges;
-        })
-        .catch(err => {
-            return err;
-        });
+const axios = require("axios");
+
+module.exports = function getLatest() {
+    return axios({
+        method: 'GET',
+        url: 'https://api.exchangeratesapi.io/latest'
+    })
+    .then((exchanges) => {
+        console.log(exchanges);
+        return exchanges;
+    })
+    .catch((err) => {
+        return {err};
+    });
 }
+
+// getLatest()
+//     .then(exchangs => {
+//         console.log(exchangs);
+//     })
